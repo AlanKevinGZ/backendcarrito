a21702221ced3431a6ab00087856f2f89a84a25f
JhonatanAGDev <201820197@tese.edu.mx>
2022-06-22 00:30:07 -0500

    Ruta mostrar productos

--- build/keys.js
+++ build/keys.js
@@ -4,7 +4,7 @@ exports.default = {
     database: {
         host: 'localhost',
         user: 'root',
-        password: '123456789',
+        password: 'admin',
         database: 'appcarrito'
     }
 };
